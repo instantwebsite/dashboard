90d45c131006405dfd3637d5fc15104a20cabe9c
victorbjelkholm <victorbjelkholm@gmail.com>
2020-10-21 16:15:48 +0200

    Quick search bar for websites
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/main/pages/websites.cljs
+++ src/main/pages/websites.cljs
@@ -1,5 +1,6 @@
 (ns pages.websites
   (:require
+    [clojure.string :refer [includes? lower-case]]
     [reagent.core :as r]
     [state :refer [app-state]]
     [auth :refer [redirect-if-no-token!]]
@@ -61,12 +62,12 @@
       :style {:display "flex"
               :justify-content "space-between"
               :align-items "center"
-              :color "#363636"
               :margin 15
+              :margin-left 0
               :padding 15
               :border-radius 3
               :background-color "rgba(246, 246, 246, 1)"
-              ::color "rgba(28, 28, 28, 1)"}}
+              :color "rgba(28, 28, 28, 1)"}}
      [:div
       {:style {:font-weight 500
                :font-size 18
@@ -166,9 +167,59 @@
     {:style {:font-size 36
              :font-weight 300
              :color "rgba(28, 28, 28, 1)"
-             :margin-left 15
-             :margin-bottom 30}}
+             :margin-left 15}}
     "Your Websites"])
+
+(defn $searchbar []
+  [:input
+   {:style {:background "#FFFFFF"
+            :border  "1px solid #C7C7C7"
+            :box-sizing "border-box"
+            :box-shadow "1px 1px 1px rgba(0, 0, 0, 0.07)"
+            :border-radius "3px"
+            :padding "5px"
+            :font-size 14
+            :margin-right 15}
+    :placeholder "Search for websites here"
+    :onChange (fn [ev]
+                (swap! app-state
+                       assoc-in
+                       [:page/websites :search-term]
+                       (-> ev .-target .-value .trim)))
+    :value (-> @app-state :page/websites :search-term)
+    :type "text"}])
+
+(defn filter-by-term [websites term]
+  (filter (fn [w]
+            (let [name (lower-case (or (:website/name w) ""))
+                  domain (lower-case (or (:website/domain w) ""))
+                  term-lc (lower-case (or term ""))]
+              (if term
+                (or (includes? name term-lc)
+                    (includes? domain term-lc))
+                true)))
+          websites))
+
+(comment
+  (def websites [{:website/name "Landing Page"
+                  :website/domain nil}
+                 {:website/name "Exit page"
+                  :website/domain "exit.instantwebsite.app"}])
+  (count
+    (filter-by-term websites nil))
+  ;; => 2
+  (count
+    (filter-by-term websites ""))
+  ;; => 2
+  (count
+    (filter-by-term websites "Landing"))
+  ;; => 1
+  (count
+    (filter-by-term websites "page"))
+  ;; => 2
+  (count
+    (filter-by-term websites "instantwebsite")))
+  ;; => 1
 
 (defn -$websites []
   (if (empty? (-> @app-state :page/websites :websites))
@@ -182,12 +233,27 @@
       ;;   :pages 4
       ;;   :state :active
       ;;   :id "w1203123"}
-     [$title]
      [:div
-       (map (fn [w]
-              ^{:key (:crux.db/id w)}
-              [$website-row2 w])
-            (-> @app-state :page/websites :websites))]]))
+       {:style {:display "flex"
+                :justify-content "space-between"
+                :align-items "center"
+                :margin-bottom 30}}
+       [$title]
+       [$searchbar]]
+     [:div
+       (let [websites (filter-by-term
+                        (-> @app-state :page/websites :websites)
+                        (-> @app-state :page/websites :search-term))]
+         (if (empty? websites)
+           [:div
+            (str "No results for search term '"
+                 (-> @app-state :page/websites :search-term)
+                 "'")]
+           (map (fn [w]
+                  ^{:key (:crux.db/id w)}
+                  [$website-row2 w])
+                websites)))]]))
+            
 
 (defn $loading []
   [:div
