92d772e3bfa6fff4f3e3806206c03d2922c6c847
Victor Bjelkholm <git@victor.earth>
2020-11-02 13:12:50 +0100

    Annotations to deploy script
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,18 @@ npx shadow-cljs release frontend
 echo "## Uploading"
 rsync -e 'ssh -p 9125' -avzP ./public/ root@176.9.1.25:/instant-website-frontend
 
+REV=$(git rev-parse HEAD)
+
+curl -XPOST https://instantwebsite.grafana.net/api/annotations \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $ANNOTATE_API_TOKEN" \
+  --data @- << EOF
+  {
+    "text": "Deployed frontend@$REV",
+    "tags": [
+      "deployment", "frontend"
+    ]
+  }
+EOF
+
 echo "DONE!"
